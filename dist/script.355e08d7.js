678a9e6ecd7ed95b5debc7843a5da35f25ce9369
Adam <adamorlowskipoland@icloud.com>
2018-07-25 23:05:14 +0200

    chore: add deploy files

---
+++ dist/script.355e08d7.js
@@ -0,0 +1,454 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"script.js":[function(require,module,exports) {
+var GAME_SPEED = 100;
+var CANVAS_BORDER_COLOUR = '#000';
+var CANVAS_BACKGROUND_COLOUR = "#fff";
+var SNAKE_COLOUR = '#98FB98';
+var SNAKE_HEAD_BORDER_COLOUR = '#006400';
+/*  Ferrari red ;-) */
+var FOOD_COLOUR = '#ff2800';
+var FOOD_BORDER_COLOUR = '#8b0000';
+
+// Get the canvas element
+var gameCanvas = document.getElementById("gameCanvas");
+var ctx = gameCanvas.getContext("2d");
+
+var snake = {
+  body: [{ x: 150, y: 150 }, { x: 140, y: 150 }, { x: 130, y: 150 }, { x: 120, y: 150 }, { x: 110, y: 150 }]
+};
+
+var food = {
+  x: 0,
+  y: 0
+};
+
+var score = 0;
+var changingDirection = false;
+
+// Horizontal velocity
+var dx = 10;
+var dy = 0;
+
+var operator = {
+  randomTen: function randomTen(min, max) {
+    return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+  },
+  didGameEnd: function didGameEnd() {
+    for (var i = 4; i < snake.body.length; i++) {
+      if (snake.body[i].x === snake.body[0].x && snake.body[i].y === snake.body[0].y) return true;
+    }
+    var hitLeftWall = snake.body[0].x < 0;
+    var hitRightWall = snake.body[0].x > gameCanvas.width - 10;
+    var hitToptWall = snake.body[0].y < 0;
+    var hitBottomWall = snake.body[0].y > gameCanvas.height - 10;
+    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+  },
+  createFood: function createFood() {
+    // Generate a random number the food x-coordinate and food y coordinate
+    food.x = this.randomTen(0, gameCanvas.width - 10);
+    food.y = this.randomTen(0, gameCanvas.height - 10);
+    // if the new food location is where the snake currently is, generate a new food location
+    snake.body.forEach(function isFoodOnSnake(part) {
+      var foodIsOnSnake = part.x === food.x && part.y === food.y;
+      if (foodIsOnSnake) operator.createFood();
+    });
+  },
+  advanceSnake: function advanceSnake() {
+    // Create the new Snake's head
+    var head = { x: snake.body[0].x + dx, y: snake.body[0].y + dy };
+    // Add the new head to the beginning of snake body
+    snake.body.unshift(head);
+    var didEatFood = snake.body[0].x === food.x && snake.body[0].y === food.y;
+    if (didEatFood) {
+      // Increase score
+      score += 10;
+      // Display score on screen
+      document.getElementById('score').innerHTML = score;
+      // Generate new food location
+      operator.createFood();
+    } else {
+      // Remove the last part of snake body
+      snake.body.pop();
+    }
+  },
+  changeDirection: function changeDirection(event) {
+    var LEFT_KEY = 37;
+    var RIGHT_KEY = 39;
+    var UP_KEY = 38;
+    var DOWN_KEY = 40;
+    if (changingDirection) return;
+    changingDirection = true;
+
+    var keyPressed = event.keyCode;
+    var goingUp = dy === -10;
+    var goingDown = dy === 10;
+    var goingRight = dx === 10;
+    var goingLeft = dx === -10;
+    if (keyPressed === LEFT_KEY && !goingRight) {
+      dx = -10;
+      dy = 0;
+    }
+
+    if (keyPressed === UP_KEY && !goingDown) {
+      dx = 0;
+      dy = -10;
+    }
+
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+      dx = 10;
+      dy = 0;
+    }
+
+    if (keyPressed === DOWN_KEY && !goingUp) {
+      dx = 0;
+      dy = 10;
+    }
+  }
+};
+
+var view = {
+  clearCanvas: function clearCanvas() {
+    //  Select the colour to fill the drawing
+    ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+    //  Select the colour for the border of the canvas
+    ctx.strokestyle = CANVAS_BORDER_COLOUR;
+    // Draw a "filled" rectangle to cover the entire canvas
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    // Draw a "border" around the entire canvas
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+  },
+  drawFood: function drawFood() {
+    ctx.fillStyle = FOOD_COLOUR;
+    ctx.strokestyle = FOOD_BORDER_COLOUR;
+    ctx.fillRect(food.x, food.y, 10, 10);
+    ctx.strokeRect(food.x, food.y, 10, 10);
+  },
+  drawSnake: function drawSnake() {
+    // loop through the snake parts drawing each part on the canvas
+    snake.body.forEach(this.drawSnakePart);
+  },
+  drawSnakePart: function drawSnakePart(snakePart) {
+    // Set the colour of the snake part
+    ctx.fillStyle = snake.body.indexOf(snakePart) === 0 ? SNAKE_HEAD_BORDER_COLOUR : SNAKE_COLOUR;
+    // Set the border colour of the snake part
+    ctx.strokestyle = SNAKE_HEAD_BORDER_COLOUR;
+    // Draw a "filled" rectangle to represent the snake part at the coordinates
+    // the part is located
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    // Draw a border around the snake part
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+  },
+  main: function main() {
+    var _this = this;
+
+    // If the game ended return early to stop game
+    if (operator.didGameEnd()) return;
+    setTimeout(function () {
+      changingDirection = false;
+      _this.clearCanvas();
+      _this.drawFood();
+      operator.advanceSnake();
+      _this.drawSnake();
+      // Call game again
+      _this.main();
+    }, GAME_SPEED);
+  }
+};
+
+view.clearCanvas();
+operator.createFood();
+view.drawFood();
+// Create the first food location
+view.drawSnake();
+
+// Call changeDirection whenever a key is pressed
+document.addEventListener("keydown", operator.changeDirection);
+
+document.addEventListener("keyup", function (event) {
+  var code = void 0;
+
+  if (event.key !== undefined) {
+    code = event.key;
+  } else if (event.keyIdentifier !== undefined) {
+    code = event.keyIdentifier;
+  } else if (event.keyCode !== undefined) {
+    code = event.keyCode;
+  }
+  code === 13 || code === 'Enter' ? view.main() : 0;
+});
+},{}],"../../../../../../usr/local/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50561' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["../../../../../../usr/local/lib/node_modules/parcel/src/builtins/hmr-runtime.js","script.js"], null)
+//# sourceMappingURL=/script.355e08d7.map
